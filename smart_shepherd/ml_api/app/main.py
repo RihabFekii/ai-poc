0dfad53f243d1f7f4be708a7f4f4905be9ac8507
Rihab <rihab.feki@fiware.org>
2022-02-18 10:02:04 +0100

    main commit

---
+++ smart_shepherd/ml_api/app/main.py
@@ -0,0 +1,55 @@
+from typing import Any
+
+from fastapi import APIRouter, FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from loguru import logger
+
+from app.config import settings
+from app.endpoints import health, predict
+
+app = FastAPI(
+    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
+    )
+
+root_router = APIRouter()
+
+
+@root_router.get("/")
+def index(request: Request) -> Any:
+    """Basic HTML response."""
+    body = (
+        "<html>"
+        "<body style='padding: 10px;'>"
+        "<h1>Welcome to the Animal Activity Prediction API</h1>"
+        "<div>"
+        "Check the docs: <a href='/docs'>here</a>"
+        "</div>"
+        "</body>"
+        "</html>"
+    )
+
+    return HTMLResponse(content=body)
+
+
+# Set all CORS enabled origins
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+app.include_router(health.router, prefix=settings.API_V1_STR)
+app.include_router(predict.router, prefix=settings.API_V1_STR)
+app.include_router(root_router)
+
+
+if __name__ == "__main__":
+    # Use this for debugging purposes only
+    logger.warning("Running in development mode. Do not run like this in production.")
+    import uvicorn
+
+    uvicorn.run("main:app", host='localhost', port=8005, log_level="info", reload=True)
