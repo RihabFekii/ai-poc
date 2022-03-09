4dd9995b2dd1c407639e408ad1b6045cfbbbf00c
Rihab <rihab.feki@fiware.org>
2022-03-09 13:04:19 +0100

    main update

--- smart_shepherd/main.py
+++ smart_shepherd/main.py
@@ -1,7 +1,38 @@
-""" dpp = DataPreProcessor()
+import requests
+
+from preprocessor.config import config
+from preprocessor.input_preprocessor import InputPreprocessor
+from preprocessor.output_preprocessor import update_prediction
+from ml_api.app.schemas.predict import PredictionResults
 
 
-#1. Nofification from CB received
-#  --> data_ready_for_model = dpp.preprocess(data)
-ml_api. """
+def predict(ip: InputPreprocessor) -> PredictionResults:
+
+	temperature = ip.get_temperature()
+	coordinates = ip.get_coordinates()
+	input_data = ip.input_preprocessor(coordinates=coordinates,temperature=temperature)
+
+	response = requests.request(
+	"POST", url=config.PREDICT_URL, 
+	headers=config.HEADER, 
+	data=input_data.json()
+	)
+
+	prediction_result = PredictionResults(**response.json())
+
+	return prediction_result
+
+
+
+if __name__ == "__main__":
+
+	ip = InputPreprocessor()
+	prediction = predict(ip)
+	print(prediction)
+	update_prediction(predictions=prediction)
+
+
+# todo
+
+# write README about the Subscription on prediction update
 
