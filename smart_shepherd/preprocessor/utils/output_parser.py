548000eec80f9a99893944740e01ce3abcfe06b6
Rihab <rihab.feki@fiware.org>
2022-03-10 11:14:48 +0100

    import path changed

--- smart_shepherd/preprocessor/utils/output_parser.py
+++ smart_shepherd/preprocessor/utils/output_parser.py
@@ -1,14 +1,14 @@
 
-from smart_shepherd.ml_api.app.schemas.predict import PredictionResults
+from ml_api.app.schemas.predict import PredictionResults
 
 
-def prediction_parser_to_ngsild(data: PredictionResults) -> dict:
+def prediction_parser_to_ngsild(data: PredictionResults):
 	prediction = data.predictions[0]
 	#prediction = {"animalActivity" : prediction}
 	prediction = {
-		"animalActivity":{
-			"type":"Property",
-			"value":prediction
+		"animalActivity": {
+			"type": "Property",
+			"value": prediction
 		}
 		}
 	return prediction
