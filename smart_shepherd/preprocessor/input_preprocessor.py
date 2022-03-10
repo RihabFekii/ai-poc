63f3659c3eee58558cfda3ea8531e3de56fd7e0d
Rihab <rihab.feki@fiware.org>
2022-03-10 11:17:07 +0100

    code cleaning

--- smart_shepherd/preprocessor/input_preprocessor.py
+++ smart_shepherd/preprocessor/input_preprocessor.py
@@ -1,4 +1,3 @@
-from termios import CINTR
 from typing import List
 
 import requests
--- smart_shepherd/preprocessor/utils/output_parser.py
+++ smart_shepherd/preprocessor/utils/output_parser.py
@@ -3,6 +3,7 @@ from ml_api.app.schemas.predict import PredictionResults
 
 
 def prediction_parser_to_ngsild(data: PredictionResults):
+
 	prediction = data.predictions[0]
 	#prediction = {"animalActivity" : prediction}
 	prediction = {
@@ -11,4 +12,5 @@ def prediction_parser_to_ngsild(data: PredictionResults):
 			"value": prediction
 		}
 		}
+		
 	return prediction
